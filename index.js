e1144bcb3cf389e948fbde6e29bd32d566f0e3af
GlaudistonNeto <glaudistonson@gmail.com>
2022-08-30 14:37:58 -0300

    ✨feature: finish validations without using lib validator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const flash = require('express-flash');
 const session = require('express-session');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 
@@ -11,17 +12,40 @@ app.use(express.urlencoded({extended: false}));
 // parse application json
 app.use(express.json());
 
+
+app.use(cookieParser('|T@LDn?@62\Rz=.q'));
 app.use(session({
   secret: 'keyword cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { maxAge: 60000 }
 }));
 
 app.use(flash());
 
 app.get('/', (req, res) =>{
-  res.render('index');
+  var emailError = req.flash('emailError');
+  var pointsError = req.flash('pointsError');
+  var nameError = req.flash('nameError');
+  var email = req.flash("email");
+  var points = req.flash('points');
+  var name = req.flash('name');
+
+  emailError = (emailError == undefined || emailError.length == 0) ? undefined :
+                emailError;
+  email = (email == undefined || email.length == 0) ? "" : email;
+
+  pointsError = (pointsError == undefined || pointsError.length == 0) ?
+                 undefined : pointsError;
+                points = (points == undefined || points.length == 0) ? "" :
+                points;
+
+  nameError = (nameError == undefined || nameError.length == 0) ? undefined :
+                nameError;
+                name = (name == undefined || name.length == 0) ? "" : name;
+
+  res.render('index', { emailError, pointsError, nameError,
+              email: email, points: points, name: name });
 });
 
 app.post("/form", (req, res) => {
@@ -32,6 +56,7 @@ app.post("/form", (req, res) => {
   var nameError;
 
   if (email == undefined || email == "") {
+
     emailError = "Email cannot be empty";
   }
 
@@ -49,6 +74,13 @@ app.post("/form", (req, res) => {
 
   if (emailError != undefined || pointsError != undefined || nameError != 
       undefined) {
+        req.flash('emailError', emailError);
+        req.flash('pointsError', pointsError);
+        req.flash('nameError', nameError);
+
+        req.flash('email', email);
+        req.flash('points', points);
+        req.flash('points', points);
         res.redirect('/');
       } else {
         res.send('You are all done!');
